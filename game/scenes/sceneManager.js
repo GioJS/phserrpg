b0d3e871b27a5e205db3d7183ccb006047e1f939
giordano cristini <giordano.cristini@exprivia.com>
2018-11-28 18:05:04 +0100

    class scene manager

--- game/scenes/sceneManager.js
+++ game/scenes/sceneManager.js
@@ -1,5 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var SceneManager = (function(){
 		function SceneManager() {
 			currentScene = 'game_boot';
@@ -11,4 +9,4 @@ var SceneManager = (function(){
 		return SceneManager;
 }());
 
-exports.LevelManager = LevelManager;
+
