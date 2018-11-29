6a0141deca923b4b26bb69b34f9c2e6ec3dfac4b
giordano cristini <giordano.cristini@exprivia.com>
2018-11-29 10:20:11 +0100

    menu start

--- game/scenes/bootScene.js
+++ game/scenes/bootScene.js
@@ -12,12 +12,42 @@ var BootScene = new Phaser.Class({
     preload: function ()
     {
         // load the resources here
+        this.load.image('finger', 'game/assets/finger.png');
+        this.cursors = this.input.keyboard.createCursorKeys();
+
     },
 
     create: function ()
     {
         //this.scene.start('WorldScene');
-        var text = this.add.text(this.cameras.main.centerX/2, this.cameras.main.centerY, "Final Phaser");
+        this.cameras.main.setBackgroundColor("#ffffff");
+
+        var text = this.add.text(this.cameras.main.centerX/2, this.cameras.main.centerY/2, "Final Phaser");
         text.setScale(1.5);
+        text.setColor("#000000");
+
+        var start =  this.add.text(this.cameras.main.centerX/2 + 20, this.cameras.main.centerY/2 + 50, "New Game");
+        start.setScale(1.0);
+        start.setColor("#000000");
+
+        var load = this.add.text(this.cameras.main.centerX/2 + 20, this.cameras.main.centerY/2 + 80, "Load Game");
+        load.setScale(1.0);
+        load.setColor("#000000");
+
+        this.cursor = this.physics.add.sprite(this.cameras.main.centerX/2, this.cameras.main.centerY/2 + 60,'finger');
+
+        /*this.physics.world.bounds.width = this.cameras.main.centerX/2 + 20;
+        this.physics.world.bounds.height = this.cameras.main.centerY/2 + 80;
+        this.cursor.setCollideWorldBounds(true);*/
+    },
+    update: function () {
+        if(this.cursors.down.isDown) {
+            this.cursor.y = this.cameras.main.centerY/2 + 90;
+        }
+        if(this.cursors.up.isDown) {
+            this.cursor.y = this.cameras.main.centerY/2 + 60;
+        }
     }
+    
+    
 });
