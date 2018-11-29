0dc7e4bedd2f90039443b2d8bfabe20158c228f1
giordano cristini <giordano.cristini@exprivia.com>
2018-11-29 10:48:28 +0100

    start game

--- game/scenes/worldScene.js
+++ game/scenes/worldScene.js
@@ -6,16 +6,12 @@ var WorldScene = new Phaser.Class({
 
         function WorldScene() {
             Phaser.Scene.call(this, {key: 'WorldScene'});
-            console.log('created')
         },
     preload: function () {
         this.centerX = this.cameras.main.centerX / 2 - 15;
         this.centerY = this.cameras.main.centerY / 2 + 30;
-        console.log('preload')
     },
     create: function () {
-        console.log('created')
-        // create your world here
         var text = this.add.text(this.centerX, this.centerY, "A long time ago...");
         text.setScale(1.2);
         text.setColor('#ffffff');
