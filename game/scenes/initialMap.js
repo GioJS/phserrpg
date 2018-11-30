24f56e7e824cbed2780184b0fe84e4b5805c8112
giordano cristini <giordano.cristini@exprivia.com>
2018-11-30 10:46:01 +0100

    refactoring movements

--- game/scenes/initialMap.js
+++ game/scenes/initialMap.js
@@ -69,59 +69,136 @@ var InitialScene = new Phaser.Class({
         this.physics.add.collider(this.player, obstacles);
 
 
-        this.up = this.physics.add.sprite(50, 160, 'up');
+        this.moveUp = false;
+        this.moveDown = false;
+        this.moveLeft = false;
+        this.moveRight = false;
+
+        this.moveUpT = false;
+        this.moveDownT = false;
+        this.moveLeftT = false;
+        this.moveRightT = false;
+
+
+        this.up = this.add.sprite(50, 160, 'up').setInteractive();
         this.up.scaleX = 0.5;
         this.up.scaleY = 0.5;
         this.up.angle = -90;
-        this.down = this.physics.add.sprite(50, 200, 'down');
+        this.up.setScrollFactor(0);
+        this.up.on('pointerdown', function(pointer, localX, localY, event){
+            this.moveUpT = true;
+        }, this);
+
+        this.up.on('pointerup', function(pointer, localX, localY, event){
+            this.moveUpT = false;
+        }, this);
+
+        this.down = this.add.sprite(50, 200, 'down').setInteractive();
         this.down.scaleX = 0.5;
         this.down.scaleY = 0.5;
         this.down.angle = +90;
-        this.left = this.physics.add.sprite(20, 180, 'left');
+
+        this.down.setScrollFactor(0);
+        this.down.on('pointerdown', function(pointer, localX, localY, event){
+            this.moveDownT = true;
+        }, this);
+
+        this.down.on('pointerup', function(pointer, localX, localY, event){
+            this.moveDownT = false;
+        }, this);
+
+        this.left = this.add.sprite(20, 180, 'left').setInteractive();
         this.left.scaleX = 0.5;
         this.left.scaleY = 0.5;
         this.left.angle = -180;
-        this.right = this.physics.add.sprite(80, 180, 'right');
+        this.left.setScrollFactor(0);
+
+        this.left.on('pointerdown', function(pointer, localX, localY, event){
+            this.moveLeftT = true;
+        }, this);
+
+        this.left.on('pointerup', function(pointer, localX, localY, event){
+            this.moveLeftT = false;
+        }, this);
+
+        this.right =  this.add.sprite(80, 180, 'right').setInteractive();
         this.right.scaleX = 0.5;
         this.right.scaleY = 0.5;
         this.right.angle = 360;
+
+        this.right.setScrollFactor(0);
+        this.right.on('pointerdown', function(pointer, localX, localY, event){
+            this.moveRightT = true;
+        }, this);
+
+        this.right.on('pointerup', function(pointer, localX, localY, event){
+            this.moveRightT = false;
+        }, this);
+
+
     },
     update: function(time, delta) {
         this.player.body.setVelocity(0);
+        this.moveUp = false;
+        this.moveDown = false;
+        this.moveLeft = false;
+        this.moveRight = false;
 
         // Horizontal movement
         if (this.cursors.left.isDown)
         {
-            this.player.body.setVelocityX(-80);
+            this.moveLeft = true;
         }
-        else if (this.cursors.right.isDown)
+        else if (this.cursors.right.isDown || this.moveRight)
         {
-            this.player.body.setVelocityX(80);
+            this.moveRight = true;
         }
 
         // Vertical movement
         if (this.cursors.up.isDown)
         {
-            this.player.body.setVelocityY(-80);
+            this.moveUp = true;
         }
         else if (this.cursors.down.isDown)
+        {
+            this.moveDown = true;
+        }
+        this.movement();
+    },
+    movement: function () {
+        // Horizontal movement
+        if (this.moveLeft || this.moveLeftT)
+        {
+            this.player.body.setVelocityX(-80);
+        }
+        else if (this.moveRight || this.moveRightT)
+        {
+            this.player.body.setVelocityX(80);
+        }
+
+        // Vertical movement
+        if (this.moveUp || this.moveUpT)
+        {
+            this.player.body.setVelocityY(-80);
+        }
+        else if (this.moveDown || this.moveDownT)
         {
             this.player.body.setVelocityY(80);
         }
         //animation
-        if (this.cursors.left.isDown)
+        if (this.moveLeft || this.moveLeftT)
         {
             this.player.anims.play('left', true);
         }
-        else if (this.cursors.right.isDown)
+        else if (this.moveRight || this.moveRightT)
         {
             this.player.anims.play('right', true);
         }
-        else if (this.cursors.up.isDown)
+        else if (this.moveUp || this.moveUpT)
         {
             this.player.anims.play('up', true);
         }
-        else if (this.cursors.down.isDown)
+        else if (this.moveDown || this.moveDownT)
         {
             this.player.anims.play('down', true);
         }
